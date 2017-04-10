bd31876d3f453531a6745f97c20e97df1e58ab83
SeanChense <chenyu380@gmail.com>
2017-04-10 15:03:12 +0800

    Update via shell robot

--- View/User.py
+++ View/User.py
@@ -62,7 +62,6 @@ def register():
 def query_orders(user):
 	orders = []
 	for order in Order.order_filter_uid(user.id):
-		print order
 		orders.append(order.as_dict())
 	return jsonify({"status":0,
 		"data":orders})
