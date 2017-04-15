cce4019106d33102532f7edd49728b2830b422a0
SeanChense <chenyu380@gmail.com>
2017-04-15 14:42:17 +0800

    Update via shell robot

--- Qiniu/image_uploader.py
+++ Qiniu/image_uploader.py
@@ -3,6 +3,7 @@
 
 from qiniu import Auth, put_file, etag, urlsafe_base64_encode
 import qiniu.config
+import uuid
 
 import sys 
 sys.path.append('..') 
@@ -15,21 +16,19 @@ secret_key = secret_config.qiniu_sk
 #构建鉴权对象
 q = Auth(access_key, secret_key)
 
-print q
+def upload_img(key, localfile):
+	#要上传的空间
+	bucket_name = 'fastorder'
+	#生成上传 Token，可以指定过期时间等
+	token = q.upload_token(bucket_name, key, 3600)
 
-#要上传的空间
-bucket_name = 'fastorder'
+	ret, info = put_file(token, key, localfile)
+	print(info)
+	print
+	print ret
 
-#上传到七牛后保存的文件名
-key = 'my-python-logo.png';
 
-#生成上传 Token，可以指定过期时间等
-token = q.upload_token(bucket_name, key, 3600)
 
-#要上传文件的本地路径
-localfile = '../test.png'
-
-ret, info = put_file(token, key, localfile)
-print(info)
-assert ret['key'] == key
-assert ret['hash'] == etag(localfile)
+# key = uuid.uuid1()
+# upload_img(key, "../test.png")
+# print 'url is http://oofm3g268.bkt.clouddn.com/'+str(key)
