1831290c9324732f03c78134c242439553bf42c1
SeanChense <chenyu380@gmail.com>
2017-05-04 14:00:05 +0800

    Add 302 for root page

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import secret_config
-from flask import Flask
+from flask import Flask, redirect
 from flaskext.mysql import MySQL
 from flask import render_template
 from Models.Restaurant import Restaurant
+from Models.Admin import Admin
 from Decorator import *
 
 
@@ -15,17 +16,24 @@ app.config['MYSQL_DATABASE_HOST'] = secret_config.database_host
 mysql.init_app(app)
 
 
-from View import User, DinnerTable, Menu, Order, Admin, RestaurantView
+from View import User, DinnerTable, Menu, Order, AdminView, RestaurantView
 
 app.register_blueprint(User.mod)
 app.register_blueprint(DinnerTable.mod)
 app.register_blueprint(Menu.mod)
 app.register_blueprint(Order.mod)
-app.register_blueprint(Admin.mod)
+app.register_blueprint(AdminView.mod)
 app.register_blueprint(RestaurantView.mod)
 
 @app.route('/', methods = ['GET'])
 def index():
+	token = request.headers.get('Cookie').split(";")[1].split("=")[1]
+	if token:
+		admin  = Admin.verify_auth_token(token)
+		if admin != -1 and \
+		   admin != -2 and \
+		   admin != -3:
+		   return redirect('/dashboard', code=302)
 	return render_template('index.html')
 
 @app.route('/dashboard', methods = ['GET'])
