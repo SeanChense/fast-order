b00e1b6dfc3f754fe1a75501b09db3f46a33a432
SeanChense <chenyu380@gmail.com>
2019-02-13 19:57:57 +0800

    Update via shell robot

--- app.py
+++ app.py
@@ -46,12 +46,20 @@ def upload_file():
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            return redirect(request.url)
+            return jsonify({
+                "status":1,
+                "msg" : "file name empty"
+            })
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return jsonify({
                 "status":0
+            })
+        else:
+            return jsonify({
+                "status":2,
+                "msg": "file name illegal"
             })
     return '''
     <!doctype html>
